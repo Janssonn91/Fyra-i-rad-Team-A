d8aab406e0c18b5f9c1d5980c84c2775a6141937
Unknown <johan_anderberg79@hotmail.com>
2017-12-06 23:38:21 +0100

    Spelregler som modal funkar med js, se beskrivning
    I navbar, använd
    <a href="#" class="nav-link nav-modal" data-toggle="modal" data-target="#exampleModal">Spelregler</a>
    så funkar det automatiskt

--- www/js/spelregler.js
+++ www/js/spelregler.js
@@ -1,49 +1,34 @@
 let theRules = $(`
-  <div class="modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p>Modal body text goes here.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary">Save changes</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h2 class="modal-title" id="exampleModalLabel">Spelregler</h2>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              4 i rad är ett spel för två spelare. Brädet består av 7 kolumner med 6 horisontella rader. Varje spelare väljer först färg och turas sedan om att släppa sina färgade brickor längst upp i valfri kolumn. Syftet är att skapa en rad med 4 av sin färg horisontellt, vertikalt eller diagonalt. Om alla kolumner är fyllda slutar spelet oavgjort.
+              <hr>
+              <ul class="youtubes pull-left">
+                <li> Länkar till filmer</li>
+                <li><a href="https://www.youtube.com/watch?v=H3FYRM9a0i4">Se hur man spelar</a></li>
+                <li><a href="https://www.youtube.com/watch?v=ONKFwqnusbI">Gör ditt eget bräde (svår)</a></li>
+                <li><a href="https://www.youtube.com/watch?v=yDWPi1pZ0Po">Mer statistik än du någonsin vill veta</a></li>
+                <li><a href="https://www.youtube.com/watch?v=rqkxmXBY7yQ">Om motståndaren är för svår, fuska</a></li>
+              </ul>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Dölj</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
   `)
 
 
-$('.rules').click(function(e){
-  //e.preventDefault();
-  $('body').append(theRules)
+$('.nav-modal').click(function(e){
+  $('main').append(theRules);
 });
 
-/*
-<div class="modal" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-*/
