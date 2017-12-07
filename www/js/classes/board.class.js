324155b412a242e56e7e5df6c3d120e14146c3e0
Petter <petter.f.andersson@gmail.com>
2017-12-07 15:13:06 +0100

    Delvist löst hoverproblemet

--- www/js/classes/board.class.js
+++ www/js/classes/board.class.js
@@ -7,9 +7,31 @@ class Board{
 						  [0,0,0,0,0,0,0],
 						  [0,0,0,0,0,0,0],
 						  [0,0,0,0,0,0,0] ];
+		this.rowHoverBrick();
 		this.createBoard();
 		this.dropBrick();
 	}
+
+	  rowHoverBrick(){
+      for(let i = 0; i < 1; i++){
+        let row = $('<div>').addClass('row row-hover-brick')
+        for(let j = 0; j < 7; j++){
+          let col = $('<div>').addClass('col col-hover-brick');
+          col.attr('data-colNr',j).attr('data-rowNr', i);
+          $(row).append(col);
+        }
+        $(this.board).append(row);
+      }
+      $(this.board).on('mouseover', '.board-column', function(){
+        let colNumber = $(this).data('colnr');
+        $(`.col-hover-brick[data-colNr='${colNumber}']`).addClass('red');
+      });
+      $(this.board).on('mouseleave', '.board-column', function(){
+        let colNumber = $(this).data('colnr');
+        $(`.col-hover-brick[data-colNr='${colNumber}']`).removeClass('red');
+      });
+    }
+
 	createBoard(){
 		for(let i = 0; i < 6; i++){
 			let row = $('<div>').addClass('row board-row');
