6fa177660ec02d2e64728fa576a9111a2b910b1f
Monika Lukic <monika.lukik@hotmail.com>
2017-12-07 21:30:08 +0100

    Fixade brädan med scale().

--- www/js/classes/board.class.js
+++ www/js/classes/board.class.js
@@ -7,36 +7,42 @@ class Board{
 						  [0,0,0,0,0,0,0],
 						  [0,0,0,0,0,0,0],
 						  [0,0,0,0,0,0,0] ];
-		this.rowHoverBrick();
 		this.createBoard();
+		this.hoverBrick();
+		this.scale();
 		this.dropBrick();
 	}
 
-	  rowHoverBrick(){
-      for(let i = 0; i < 1; i++){
-        let row = $('<div>').addClass('row row-hover-brick')
-        for(let j = 0; j < 7; j++){
-          let col = $('<div>').addClass('col col-hover-brick');
-          col.attr('data-colNr',j).attr('data-rowNr', i);
-          $(row).append(col);
-        }
-        $(this.board).append(row);
-      }
-      $(this.board).on('mouseover', '.board-column', function(){
-        let colNumber = $(this).data('colnr');
-        $(`.col-hover-brick[data-colNr='${colNumber}']`).addClass('red');
-      });
-      $(this.board).on('mouseleave', '.board-column', function(){
-        let colNumber = $(this).data('colnr');
-        $(`.col-hover-brick[data-colNr='${colNumber}']`).removeClass('red');
-      });
-    }
+	scale(){
+		let boardW = 798;
+		let boardH = 909;
+		let w = $(window).width();
+		let h = $(window).height(); 
+		h -= $('header').outerHeight() + $('footer').outerHeight();
+		w -= + 20 * 2; 
+		let wScale = w / boardW; 
+		let hScale = h / boardH;
+		let scaling = Math.min(wScale, hScale);
+		$('#board').css('transform', `scale(${scaling})`).show();
+		$('.board-holder').width(boardW * scaling).height(boardH * scaling);
+	}
 
 	createBoard(){
+		//transparent row/col for hover brick
+		let hoverRow = $('<div>').addClass('hover-brick-row');
+		for(let j = 0; j < 7; j++){
+			let hoverCol = $('<div>').addClass('hover-brick-col');
+			hoverCol.attr('data-colNr',j).attr('data-rowNr', 0);
+			let circles = $('<div>').addClass('circle');
+			hoverCol.append(circles);
+			$(hoverRow).append(hoverCol);
+		}
+		$(this.board).append(hoverRow);
+		// visible row/col
 		for(let i = 0; i < 6; i++){
-			let row = $('<div>').addClass('row board-row');
+			let row = $('<div>').addClass('board-row');
 			for(let j = 0; j < 7; j++){
-				let col = $('<div>').addClass('col board-column noBrick');
+				let col = $('<div>').addClass('board-col noBrick');
 				col.attr('data-colNr',j).attr('data-rowNr', i);
 				let circles = $('<div>').addClass('circle');
 				col.append(circles);
@@ -46,9 +52,20 @@ class Board{
 		}
 	}
 
+	hoverBrick(){
+		$(this.board).on('mouseover', '.board-col', function(){
+			let colNumber = $(this).data('colnr');
+			$(`.hover-brick-col[data-colNr='${colNumber}']`).children().addClass('red');
+		});
+		$(this.board).on('mouseleave', '.board-col', function(){
+			let colNumber = $(this).data('colnr');
+			$(`.hover-brick-col[data-colNr='${colNumber}']`).children().removeClass('red');
+		});
+	}
+
 	dropBrick(){
 		const that = this;
-		$(this.board).on('click', '.board-column', function(){
+		$(this.board).on('click', '.board-col', function(){
 			let colNumber = $(this).data('colnr');
 			let emptyCols = $(`.noBrick[data-colNr='${colNumber}']`);
 			for(let i = emptyCols.length - 1; i>= 0; i--){
