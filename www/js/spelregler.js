38f40c2cb82db65a6fedc4b7524bc74641943355
Niklas Jansson <Niklas.Jansson91@hotmail.com>
2017-12-08 09:58:33 +0100

    Ändrat och fixat till i spelregler.

--- www/js/spelregler.js
+++ www/js/spelregler.js
@@ -3,16 +3,16 @@ let theRules = $(`
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h2 class="modal-title" id="exampleModalLabel">Spelregler</h2>
+              <h2 class="modal-title text-danger" id="exampleModalLabel">Spelregler</h2>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body mx-4">
               4 i rad är ett spel för två spelare. Brädet består av 7 kolumner med 6 horisontella rader. Varje spelare väljer först färg och turas sedan om att släppa sina färgade brickor längst upp i valfri kolumn. Syftet är att skapa en rad med 4 av sin färg horisontellt, vertikalt eller diagonalt. Om alla kolumner är fyllda slutar spelet oavgjort.
               <hr>
-              <ul class="youtubes pull-left">
-                <li> Länkar till filmer</li>
+              <ul class="youtubes pl-0">
+                <h4 class="text-danger">Länkar till filmer</h4>
                 <li><a href="https://www.youtube.com/watch?v=H3FYRM9a0i4">Se hur man spelar</a></li>
                 <li><a href="https://www.youtube.com/watch?v=ONKFwqnusbI">Gör ditt eget bräde (svår)</a></li>
                 <li><a href="https://www.youtube.com/watch?v=yDWPi1pZ0Po">Mer statistik än du någonsin vill veta</a></li>
