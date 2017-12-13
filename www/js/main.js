86c150fe42eaaf6c64e807ace4d1881ae92504d3
Johan Anderberg <johan_anderberg79@hotmail.com>
2017-12-13 13:58:13 +0100

    Fixat med att läsa av värde för spelare, inte klart än

--- www/js/main.js
+++ www/js/main.js
@@ -13,20 +13,25 @@ $('#player1-btn').click(function(){
   if(name === ''){
     return;
   };
-  alert(name);
-  let player1 = new Human('app', name, 'red');
-  alert(player1.color);
+  // avläs värde i dropdown, måste sätta det på selectorn istället för bara ett värde, annars blir det alltid 1 här...
+  let value = $('#player1opt1').val();
+  let player1;
+  if(){
+  player1 = new Human('app', name, 'red');
+  }
+  else{
+  	player1 = new Computer('app', name, 'red');
+  }
+  alert(player1.name + value);
 })
 
 
-$('#player1-btn').click(function(){
+$('#player2-btn').click(function(){
 	// lägg formulärets värde i name
-  let name = ($('#player1').val());
+  let name2 = ($('#player2').val());
   // om inget är ifyllt, return
-  if(name === ''){
+  if(name2 === ''){
     return;
   };
-  alert(name);
-  let player1 = new Human('app', name, 'red');
-  alert(player1.color);
+  let player2 = new Human('app', name2, 'yellow');
 })
