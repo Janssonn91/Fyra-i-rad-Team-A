7dd66618e324d608d0c93f9b1a7a0e847825acc6
Johan Anderberg <johan_anderberg79@hotmail.com>
2017-12-14 00:05:34 +0100

    Läser av formulär och skapar human/computer class
    Vet ej hur den ska ligga i game för att fungera så den är utkommenterad helt och hållet

--- www/js/classes/game.class.js
+++ www/js/classes/game.class.js
@@ -13,4 +13,45 @@ class Game {
   	
   	}
 
+  /*$('#player1-btn').click(function(){
+  // lägg formulärets värde i name
+  let name = ($('#player1').val());
+  console.log(name + 'först')
+  // om inget är ifyllt, return
+  if(name === ''){
+    return;
+  }
+
+    if($('#exampleRadios1').is(':checked')) { 
+      let player1;
+      player1 = new Human('app', name, 'red');
+  }
+  else{
+    let player1;
+      player1 = new Computer('app', name, 'red');
+    }
+});
+
+
+$('#player2-btn').click(function(){
+  // lägg formulärets värde i name
+  let name = ($('#player2').val());
+  console.log(name + 'först')
+  // om inget är ifyllt, return
+  if(name === ''){
+    return;
+  }
+
+    if($('#exampleRadios3').is(':checked')) { 
+      let player2;
+      player2 = new Human('app', name, 'yellow');
+  }
+  else{
+    let player1;
+      player2 = new Computer('app', name, 'yellow');
+    }
+});*/
+
+
+
 }
--- www/js/main.js
+++ www/js/main.js
@@ -3,32 +3,3 @@ let game = new Game();
 $(window).resize(game.board.scale);
 
 
-$('#player1-btn').click(function(){
-	// lägg formulärets värde i name
-  let name = ($('#player1').val());
-  // om inget är ifyllt, return
-  if(name === ''){
-    return;
-  }
-  // avläs värde i dropdown, måste sätta det på selectorn istället för bara ett värde, annars blir det alltid 1 här...
-  let value = $('#player1opt1').val();
-  let player1;
-  if(){
-  player1 = new Human('app', name, 'red');
-  }
-  else{
-  	player1 = new Computer('app', name, 'red');
-  }
-  alert(player1.name + value);
-});
-
-
-$('#player2-btn').click(function(){
-	// lägg formulärets värde i name
-  let name2 = ($('#player2').val());
-  // om inget är ifyllt, return
-  if(name2 === ''){
-    return;
-  };
-  let player2 = new Human('app', name2, 'yellow');
-});
