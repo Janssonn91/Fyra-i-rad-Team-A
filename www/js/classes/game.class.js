76faa38ca1ea38fcfaa9df66afe28496bb5b642d
Johan Anderberg <johan_anderberg79@hotmail.com>
2017-12-14 12:38:55 +0100

    Konfilktlösning

--- www/js/classes/game.class.js
+++ www/js/classes/game.class.js
@@ -1,11 +1,12 @@
 class Game {
 
   constructor(){
-    this.board = new Board('#board',this);
+    //this.board = new Board('#board',this);
     this.player = new Player(this);
     this.currentPlayer;
     this.counter;
     this.victoryLoop();
+    this.createPlayer(name);
   }
 
   	victoryLoop(){
@@ -16,7 +17,6 @@ class Game {
   /*$('#player1-btn').click(function(){
   // lägg formulärets värde i name
   let name = ($('#player1').val());
-  console.log(name + 'först')
   // om inget är ifyllt, return
   if(name === ''){
     return;
@@ -36,7 +36,6 @@ class Game {
 $('#player2-btn').click(function(){
   // lägg formulärets värde i name
   let name = ($('#player2').val());
-  console.log(name + 'först')
   // om inget är ifyllt, return
   if(name === ''){
     return;
@@ -52,6 +51,34 @@ $('#player2-btn').click(function(){
     }
 });*/
 
+createPlayer(name){
+if(name === ''){
+    return;
+  };
 
+  if($('#exampleRadios1').is(':checked')) { 
+    let player1;
+    player1 = new Human('app', name, 'red');
+  }
+  else{
+    let player1;
+    player1 = new Computer('app', name, 'red');
+    }
+
+  if($('#exampleRadios3').is(':checked')) { 
+    let player2;
+    player2 = new Human('app', name, 'yellow');
+      if(player2.name){
+    alert(player2.name + ' är human ' + player2.color);
+    }
+  }
+  else{
+    let player2;
+    player2 = new Computer('app', name, 'yellow');
+    alert(player2.name + ' är dator ' + player2.color);
+    }
+};
 
 }
+
+
