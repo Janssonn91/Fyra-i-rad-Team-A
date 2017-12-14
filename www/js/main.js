41fe320b81d57c555ed5fa74ceb4da51bc907f72
Johan Anderberg <johan_anderberg79@hotmail.com>
2017-12-14 12:37:13 +0100

    Konfliktlösning

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,17 @@
 let game = new Game();
 
 
+$('#player1-btn').click(function(){
+  // lägg formulärets värde i name
+  let name = ($('#player1').val());
+  // om inget är ifyllt, return
+  game.createPlayer(name);
+});
 
+$('#player2-btn').click(function(){
+  // lägg formulärets värde i name
+  let name = ($('#player2').val());
+  // om inget är ifyllt, return
+  game.createPlayer(name);
+});
 
