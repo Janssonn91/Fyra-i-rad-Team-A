60ab13ba08e62c55b26978a387e94a322dcced2f
Niklas Jansson <Niklas.Jansson91@hotmail.com>
2017-12-19 14:12:00 +0100

    Påbörjan på en modalpop-up med metodanvändning.

--- www/js/classes/game.class.js
+++ www/js/classes/game.class.js
@@ -53,7 +53,8 @@ class Game {
     */
     let gameover = winner ? winner : (!emptySlots ? 'draw' : false);
       if (gameover == this.currentPlayer.name){
-       $('#win').modal();
+        this.checkForVictory();
+       // this.win.checkForVictory(this.roundNumber, this.currentPlayer.name);
        that.highscore.savePlayer(this.roundNumber, this.currentPlayer.name);               
       } 
       else if(gameover == "draw"){
@@ -64,6 +65,35 @@ class Game {
         return false;
       }
   }// victoryLoop
+
+    checkForVictory(roundNumber, name){
+    let win = $(`
+      <div class="modal fade" id="win" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h2 class="modal-title text-danger">Grattis!</h2>
+              </div>
+              <div class="modal-body mx-4">
+                <p>Spelare ${"name"} har vunnit!</p>
+                <p> Det gick ${"roundNumber"} rundor till vinst.</p>
+                <p>Spela igen? Tryck på knappen.
+              </div>
+              <div class="modal-footer">
+                <a href="/Namn.html"><button type="button" class="btn btn-danger" data-dismiss="modal">Spela igen</button></a>
+              </div>
+            </div>
+          </div>
+        </div>
+    `);
+    // if(window.location.pathname == '/spela.html'){
+      // do {
+        $('main').append(win);
+      // console.log("Hejsan")
+    // };
+      // }
+    }
+    
 
   createPlayer(){
     $(document).on('click', '.button', function(){
--- www/js/win.js
+++ www/js/win.js
@@ -1,23 +1,32 @@
-let win = $(`
-    <div class="modal fade" id="win" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h2 class="modal-title text-danger" id="exampleModalLabel">Grattis!</h2>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body mx-4">
-             
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Stäng</button>
-            </div>
-          </div>
-        </div>
-      </div>
-  `)
-  if(window.location.pathname == '/spela.html'){
-    $('main').append(win);
-  };
+// class Win{
+//   constructor(game){
+//     this.game = game;
+//   }
+//   // checkForVictory(roundNumber, name){
+//   //   let win = $(`
+//   //     <div class="modal fade" id="win" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//   //         <div class="modal-dialog" role="document">
+//   //           <div class="modal-content">
+//   //             <div class="modal-header">
+//   //               <h2 class="modal-title text-danger">Grattis!</h2>
+//   //             </div>
+//   //             <div class="modal-body mx-4">
+//   //               <p>Spelare ${"name"} har vunnit!</p>
+//   //               <p> Det gick ${"roundNumber"} rundor till vinst.</p>
+//   //               <p>Spela igen? Tryck på knappen.
+//   //             </div>
+//   //             <div class="modal-footer">
+//   //               <a href="/Namn.html"><button type="button" class="btn btn-danger" data-dismiss="modal">Spela igen</button></a>
+//   //             </div>
+//   //           </div>
+//   //         </div>
+//   //       </div>
+//   //   `);
+//   //   // if(window.location.pathname == '/spela.html'){
+//   //     do {
+//   //       $('main').append(win);
+//   //     console.log("Hejsan")
+//   //   // };
+//   //     }
+//   // }
+// }
